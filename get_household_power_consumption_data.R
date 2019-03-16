9910d510558524288a4128e003f8059b7e1f038b
C Maroto <charlie.maroto@gmail.com>
2019-03-16 10:19:52 -0700

    Changed script that gets data to write a file with the only data from the 2 days we need for this project, resulting data file added to source control too

--- get_household_power_consumption_data.R
+++ get_household_power_consumption_data.R
@@ -8,17 +8,31 @@ if (!file.exists("data")) {
 }
 
 # Set file name for source data
-sourcedata <- paste(getwd(), "/data/household_power_consumption.zip", sep = "")
+all_data <- paste(getwd(), "/data/household_power_consumption.zip", sep = "")
 
 # Check if source data has been downloaded, else get it
 # For some unknown reason to me, !file.exists didn't work with the file as it did for folders
 # so comparing output to TRUE instead
-if (!file.exists(sourcedata)) {
+if (!file.exists(all_data)) {
     # Get data and unzip it
     message("Source data file not found, begining download...")
-    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", sourcedata)
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", all_data)
     message("Source data file downloaded, begining unzip...")
-    unzip(sourcedata, exdir = "./data")
+    unzip(all_data, exdir = "./data")
     message("Source data file unzipped.")
 }
 
+# Set file name for source data
+source_file <- paste(getwd(), "/data/household_power_consumption.txt", sep = "")
+
+# Read all data from file, added vector for na which includes "?" known to be used
+# in this data set for missing data
+all_data <- read_delim(file = source_file, delim = ";", na = c("", "NA", "?"))
+# Change the type of the Date variable from character to date 
+all_data$Date <- as.Date(all_data$Date, format = "%d/%m/%Y")
+
+# Limit data to Feb 1st and 2nd of 2017, since those are the only days needed for analysis
+all_data <- all_data %>%  filter(Date >= as.Date('2007-02-01') & Date <= as.Date('2007-02-02'))
+
+# Write the two days filtered data to be used for plotting in other scripts
+write_csv(all_data, path = paste(getwd(), "/data/household_power_consumption_feb1_2-2017.csv", sep = ""))
--- plot1.R
+++ plot1.R
@@ -3,8 +3,7 @@
 ## March 2019
 
 # Load libraries to be used
-#library(plyr)
-#library(readr)
+library(tidyverse)
 
 # Create folder for data, if it doesn't exist
 if (!file.exists("data")) {
@@ -12,12 +11,13 @@ if (!file.exists("data")) {
 }
 
 # Set file name for source data
-sourcedata <- paste(getwd(), "/data/household_power_consumption.zip", sep = "")
+input_file <- paste(getwd(), "/data/household_power_consumption_feb1_2-2017.csv", sep = "")
 
 # Check if source data has been downloaded, else get it
-if (!file.exists(sourcedata)) {
+if (!file.exists(input_file)) {
     # Get data and unzip it
     source("get_household_power_consumption_data.R")
 }
 
-
+# Load data from file
+source_data <- read_csv(input_file)
